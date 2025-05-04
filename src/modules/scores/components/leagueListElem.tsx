a0547cecfc03e6713c09b687541719c7a75affd0
Ek4m <elvin.james.99@gmail.com>
2025-05-04 18:51:45 +0400

    UI:clickable league list item

--- src/modules/scores/components/leagueListElem.tsx
+++ src/modules/scores/components/leagueListElem.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
 import React from "react";
 import { FaRegStar } from "react-icons/fa";
 
 export const LeagueListElement = () => {
   return (
-    <div className="flex justify-between bg-[#181818] p-2 rounded-lg !mb-2">
+    <Link
+      href="/game/1"
+      className="flex justify-between bg-[#181818] py-2 pl-4 pr-3 rounded-lg !mb-1 !relative overflow-hidden h-[70px] hover:bg-[#414141] cursor-pointer"
+    >
+      <div className="!absolute rounded-md w-[10px] h-[56px] bg-orange-500 left-[-5px] top-[7px]"></div>
       <div className="flex items-center">
         <h6 className="text-orange-400 text-xs font-semibold">56`</h6>
         <div className="flex flex-col items-start !ml-2">
@@ -13,7 +18,7 @@ export const LeagueListElement = () => {
               src="https://lsm-static-prod.livescore.com/medium/enet/9937.png"
               alt="team icon"
             />
-            <p className="text-[#cacaca]">Brentford</p>
+            <p className="text-[#cacaca] text-sm">Brentford</p>
           </div>
           <div className="flex items-center">
             <img
@@ -21,7 +26,7 @@ export const LeagueListElement = () => {
               src="https://lsm-static-prod.livescore.com/medium/enet/10260.png"
               alt="team icon"
             />
-            <p className="text-[#cacaca]">Manchester United</p>
+            <p className="text-[#cacaca] text-sm">Manchester United</p>
           </div>
         </div>
       </div>
@@ -34,6 +39,6 @@ export const LeagueListElement = () => {
           <FaRegStar color="#cacaca" size={20} />
         </button>
       </div>
-    </div>
+    </Link>
   );
 };
