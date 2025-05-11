1804aa1482ad987bae97208ee613aff416fd87d8
Ek4m <elvin.james.99@gmail.com>
2025-05-11 16:30:02 +0400

    UI:back button on left aside

--- src/modules/scores/components/leftAside.tsx
+++ src/modules/scores/components/leftAside.tsx
@@ -6,14 +6,25 @@ import { FilterList } from "./filterList";
 import { CountriesList } from "./countriesList";
 import { ScoresContext } from "../contexts";
 import { LeaguesListByCountry } from "./leaguesList";
+import { IoIosArrowBack } from "react-icons/io";
 
 export const ScoreLeftAside = () => {
-  const { countryId } = useContext(ScoresContext);
-  console.log("COUNTRY ID", countryId);
+  const { countryId, setCountryId } = useContext(ScoresContext);
 
   return (
     <div className="rounded-lg border-1 border-[#414141] lg:block hidden h-[max-content]">
-      <AsideSearch />
+      {!!countryId ? (
+        <div
+          onClick={() => setCountryId("")}
+          role="button"
+          className="border-b-1 border-[#414141] p-3 flex items-center cursor-pointer"
+        >
+          <IoIosArrowBack color="white" />
+          <p className="text-white text-sm !ml-1">Select league</p>
+        </div>
+      ) : (
+        <AsideSearch />
+      )}
       <div className="w-full p-2 !mt-3">
         {!!countryId ? (
           <FilterList title="Competitions">
